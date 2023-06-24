6baa839016a89429e3a772ec2dbc75f35433505c
ryukyunghye <ryukyung123@naver.com>
2023-06-25 01:15:26 +0900

    :lipstick: (design/theme/#1): 테마(팔레트) 생성

---
+++ src/assets/theme.js
@@ -0,0 +1,28 @@
+import '../index.css';
+const color = {
+  white: '#ffffff',
+  lightGray: '#E1E1E1',
+  darkGray: '#1F2528',
+  black: '#000000',
+  opacity: 'rgba(0, 0, 0, 0.10)',
+};
+const font = {
+  default: 'KOTRAHOPE',
+  user: 'KyoboHandwriting2021sjy',
+};
+const weight = {
+  bold: '600',
+  semi: '500',
+  regular: '400',
+};
+const shadow = {
+  default: `box-shadow: 0px 4px 4px 0px ${common.color.opacity}`,
+};
+const radius = {
+  rounder: '25px',
+  round: '10px',
+};
+
+const theme = { color, font, weight, shadow, radius };
+
+export default theme;
